90e41a5ca8e5098f6a74b97adb94b88a696b7d3c
mohamedmurshidh.vc <mdmurshidh1@gmail.com>
2026-02-17 20:06:22 +0530

    resources addition

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+MERGE INTO operation_types (operation_type_id, description) KEY (operation_type_id) VALUES (1, 'CASH PURCHASE');
+MERGE INTO operation_types (operation_type_id, description) KEY (operation_type_id) VALUES (2, 'INSTALLMENT PURCHASE');
+MERGE INTO operation_types (operation_type_id, description) KEY (operation_type_id) VALUES (3, 'WITHDRAWAL');
+MERGE INTO operation_types (operation_type_id, description) KEY (operation_type_id) VALUES (4, 'PAYMENT');
